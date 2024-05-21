bc9be5f82182fd55d2a4cdbd70ae3f38a1f22839
Ido Gutman <idogut3@gmail.com>
2024-05-21 19:08:20 +0300

    fixed a bug, now dealing with another one which wont let me create a new activity ~ido

--- app/src/main/java/com/example/desmosclonejavafirst/GraphingCalculatorActivity.java
+++ app/src/main/java/com/example/desmosclonejavafirst/GraphingCalculatorActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class graphingCalculator extends AppCompatActivity {
+public class GraphingCalculatorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
